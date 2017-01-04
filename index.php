a5f6c62a71be74dcb9d0bd8cb2794798e8b25e71
oeduardoal <eduardoalmeida258@gmail.com>
2017-01-04 16:08:05 -0300

    Other

--- index.php
+++ index.php
@@ -6,5 +6,5 @@
 	<?php get_template_part( 'templates/video', 'tpl' ); ?>
 	<?php get_template_part( 'templates/slide-eventos', 'tpl' ); ?>
 	<?php get_template_part( 'templates/conheca-nossos-cursos', 'tpl' ); ?>
-	<!-- Edu -->
+	<!-- Other -->
 <?php get_footer(); ?>
